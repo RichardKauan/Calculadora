7dfcd8a7d7401c57dba2d2c5ff4252279666256f
RichardKauan <richardkauan0@gmail.com>
2019-08-06 18:59:40 +0900

    JavaScript quase finalizado com adições em HTML e CSS

--- www/components/calc.js
+++ www/components/calc.js
@@ -1,4 +1,45 @@
 function transfereOne(){
   var one = document.getElementById("one").value;
   document.getElementById("tela").value = one;
+};
+
+function transfereTwo(){
+  var two = document.getElementById("two").value;
+  document.getElementById("tela").value = two;
 }
+
+function transfereThree(){
+  var three = document.getElementById("three").value;
+  document.getElementById("tela").value = three;
+};
+
+function transfereFour(){
+  var four = document.getElementById("four").value;
+  document.getElementById("tela").value = four;
+};
+
+function transfereFive(){
+  var five = document.getElementById("five").value;
+  document.getElementById("tela").value = five;
+};
+
+function transfereSix(){
+  var six = document.getElementById("six").value;
+  document.getElementById("tela").value = six;
+};
+
+function transfereSeven(){
+  var seven = document.getElementById("seven").value;
+  document.getElementById("tela").value = seven;
+};
+
+function transfereEight(){
+  var eight = document.getElementById("eight").value;
+  document.getElementById("tela").value = eight;
+};
+
+function transfereNine(){
+  var nine = document.getElementById("nine").value;
+  document.getElementById("tela").value = nine;
+}
+
